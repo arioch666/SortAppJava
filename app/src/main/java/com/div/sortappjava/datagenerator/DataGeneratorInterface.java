9ceccf601b07315bb1802a6787b19d69b4e1fd97
arioch666 <divij.durve@gmail.com>
2017-11-13 15:10:39 -0800

    Comments for the IntegerDataGenerator

--- app/src/main/java/com/div/sortappjava/datagenerator/DataGeneratorInterface.java
+++ app/src/main/java/com/div/sortappjava/datagenerator/DataGeneratorInterface.java
@@ -1,7 +1,12 @@
 package com.div.sortappjava.datagenerator;
 
+import static com.div.sortappjava.utils.Constants.DataGenerator.RANDOM;
+
 /**
  * Created by arioch666 on 11/13/17.
+ *
+ * All data generators present and future should use implement this interface to allow for seamless
+ * replacement of the Data Generator at any point without making any other changes.
  */
 
 public interface DataGeneratorInterface {
